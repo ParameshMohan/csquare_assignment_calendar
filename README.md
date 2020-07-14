6b8978c7ac1aa48319b749d3737823097ff0e469
Paramesh Mohan <paaru18@gmail.com>
2020-07-14 18:00:54 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+
+cd fullcalendar-example-projects/react-redux
+npm install
+```
+
+
+## Build Commands
+
+```bash
+npm run start # builds and opens a web browser
+
+# other commands:
+npm run build # builds files into dist/ directory
+npm run watch # same as build, but watches for changes
+npm run clean # start fresh
+```
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+import rootReducer from './reducer'
+import App from './App'
+import './index.css'
+
+let store = createStore(rootReducer, applyMiddleware(thunk))
+
+document.addEventListener('DOMContentLoaded', function() {
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.body.appendChild(document.createElement('div'))
+  )
+})
